c802eaa53fe553eb84eabcfb822e476b07aa2f3d
alperergul <alper.ergul.97@gmail.com>
2023-10-14 15:52:19 +0300

    mongodb added

--- restful-api/web/app.py
+++ restful-api/web/app.py
@@ -1,11 +1,30 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 
-
+from pymongo import MongoClient
 
 app = Flask(__name__)
 api = Api(app)
 
+client = MongoClient("mongodb://db:27017")
+db = client.aNewDB
+UserNum = db["UserNum"]
+
+UserNum.insert_one({
+    'num_of_users': 0
+})
+
+class Visit(Resource):
+    def get(self):
+        prev_num = UserNum.find({})[0]['num_of_users']
+        new_num = prev_num + 1
+        UserNum.update_one({}, {"$set": {"num_of_users": new_num}})
+        return str("Hello user " + str(new_num))
+    
+class Reset(Resource):
+    def get(self):
+        UserNum.update_one({}, {"$set": {"num_of_users": 0}})
+        return str("Success!")
 
 def checkPostedData(postedData, functionName):
     if(functionName == "add" or functionName == "subtract" or functionName == "multiply"):
@@ -24,9 +43,7 @@ def checkPostedData(postedData, functionName):
 class Add(Resource):
 
     def post(self):
-
         postedData = request.get_json()
-
         statusCode = checkPostedData(postedData, "add")
 
         if statusCode != 200:
@@ -49,9 +66,6 @@ class Add(Resource):
         }
 
         return jsonify(retMap)
-
-
-
 
 class Subtract(Resource):
     def post(self):
@@ -141,12 +155,13 @@ api.add_resource(Add, "/add")
 api.add_resource(Subtract, "/subtract")
 api.add_resource(Multiply, "/multiply")
 api.add_resource(Divide, "/division")
+api.add_resource(Visit, "/hello")
+api.add_resource(Reset, "/reset")
 
 
 @app.route('/')
 def hello_world():
     return "Hello World!"
-
 
 if __name__=="__main__":
     app.run(host='0.0.0.0')
